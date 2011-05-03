abfdcae90ce116e030e35f75a4c887e9fdb84702
Wei Zhuo <weizhuo@gmail.com>
2011-05-03 12:45:49 +1000

    initial commit

---
+++ Classes/staleAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  staleAppDelegate.h
+//  stale
+//
+//  Created by Wei Zhuo on 3/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface staleAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
